4ca50a66fae2c8addaf67a4b51cfb540d7e7db74
jicaey <michaelyoung.dev@gmail.com>
2017-12-21 13:40:22 -0500

    First commit

---
+++ PoliDO/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  PoliDO
+//
+//  Created by Michael Young on 12/19/17.
+//  Copyright © 2017 Michael Young. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    
+    static let background = UIColor(red:0.09, green:0.11, blue:0.14, alpha:1.0)
+    static let redAccent = UIColor(red:0.73, green:0.00, blue:0.00, alpha:1.0)
+    static let blueAccent = UIColor(red:0.34, green:0.61, blue:0.67, alpha:1.0)
+}
---
+++ PoliDO/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  PoliDO
+//
+//  Created by Michael Young on 12/19/17.
+//  Copyright © 2017 Michael Young. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
